63ba6d2a5a4bff1b131595c3c2d06ac5669c99ea
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-02-25 15:37:08 +1100

    Create Solution.java

---
+++ Algorithms/Warmup/Solve Me First/Solution.java
@@ -0,0 +1,7 @@
+// Author: Atharv Damle
+// Function to return the sum of two elements
+// Full Question: https://www.hackerrank.com/challenges/solve-me-first/submissions/code/87503189
+
+static int solveMeFirst(int a, int b) {
+      	return (a+b);
+}
