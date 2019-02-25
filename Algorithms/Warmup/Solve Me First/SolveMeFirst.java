c8b1ef33e920f65774791512be138c68dbd792b2
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-02-25 15:42:31 +1100

    Update and rename Solution.java to SolveMeFirst.java

--- Algorithms/Warmup/Solve Me First/SolveMeFirst.java
+++ Algorithms/Warmup/Solve Me First/SolveMeFirst.java
@@ -1,6 +1,6 @@
 // Author: Atharv Damle
 // Function to return the sum of two elements
-// Full Question: https://www.hackerrank.com/challenges/solve-me-first/submissions/code/87503189
+// Full Question: https://www.hackerrank.com/challenges/solve-me-first/problem
 
 static int solveMeFirst(int a, int b) {
       	return (a+b);
